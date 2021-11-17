3c8fcca4ad994e22fa71cf1d3aeb2503774f8fd4
CallumOrmond <callumormond10@gmail.com>
2021-11-17 15:34:37 +0000

    Added Django

---
+++ WikiSearch-master/mysite/wiki/views.py
@@ -0,0 +1,27 @@
+
+
+# Create your views here.
+
+
+import json
+from django.http import JsonResponse , HttpResponse ####
+from . import test
+
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the wiki index.")
+
+
+# https://pypi.org/project/wikipedia/#description
+def get_wiki_summary(request):
+    topic = request.GET.get('topic', None)
+
+    print('topic:', topic)
+
+    data = {}
+
+    #test.readURL(topic)
+
+
+    return JsonResponse(data)
