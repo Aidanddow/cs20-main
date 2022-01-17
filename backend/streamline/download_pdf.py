3bfa9a6f8d89d4df0d7b2778af7d913ecb24a060
Aidanddow <2468915D@student.gla.ac.uk>
2022-01-17 11:32:49 +0000

    Added main method in download_pdf

--- backend/streamline/download_pdf.py
+++ backend/streamline/download_pdf.py
@@ -1,35 +1,52 @@
-import urllib.request
+'''
+Download pdf tables from terminal with
 
+% python download_pdf.py <filename>
+'''
+
+import urllib.request
+import camelot 
+import sys
+import os
+
+# Need to mkdir files folder in current directory to work
 FOLDER = "files"
 
 #Simple script to download a pdf from a link - wont be used in project, just used 
-#to make sure the pdf if got, no filename handling
+#to make sure the pdf is got, no filename handling
 def download_file(download_url, pdf_name):
     response = urllib.request.urlopen(download_url)    
     pdf_path = os.path.join(FOLDER, pdf_name) 
 
     print("--- Downloading pdf")
         
-    file = open(pdf_path, 'wb')
-    file.write(response.read())
-    file.close()
- 
-import camelot 
-import os
+    with open(pdf_path, 'wb') as file:
+        file.write(response.read())
+    
+    return pdf_path
 
-def get_csv_table_from_pdf(pdf_name, pages="all"):
 
-    pdf_path = os.path.join(FOLDER, pdf_name) 
-    csv_path = os.path.join(FOLDER, "table.csv") 
-
-    print("--- Checking for tables")
-
+def download_pdf_tables(pdf_path, pages="all"):
+    print("--- Checking for tables") 
     tables = camelot.read_pdf(pdf_path, pages=pages, flavor="stream", edge_tol=100)
 
-    if(tables.n>0):
-        print("--- Saving tables to CSV")
+    if len(tables) > 0:
+        print(f"--- Saving {len(tables)} tables to CSV")
+        csv_path = os.path.join(FOLDER, "table.csv")
         tables.export(csv_path, f='csv', compress=False)
+        print(f"--- CSV files saved to {csv_path}")
     else:
         print("--- No tables found")
-    
-    return
+
+
+
+# Temporary main method to download pdf tables from terminal
+if __name__ == "__main__":
+    try:
+        pdf = sys.argv[1]
+        download_pdf_tables(pdf)
+    except IndexError:
+        print("No pdf file provided")
+    except Exception as e:
+        print("Error", e)
+
--- backend/streamline/views.py
+++ backend/streamline/views.py
@@ -1,55 +1,42 @@
-
-
-# Create your views here.
-
-
-from datetime import date
 import json
+import os
 from django.http import JsonResponse , HttpResponse ####
 from . import extract, download_image, download_pdf
 
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the wiki index.")
+    return HttpResponse("Hello World! Server is up and running")
 
 
-# https://pypi.org/project/wikipedia/#description
 def get_page_data_HTML(request):
     topic = request.GET.get('topic', None)
-
     print('topic-HTML:', topic)
-
-    data = {}
-
     extract.extract(topic)
 
+    data = {}
+    return JsonResponse({data})
 
-    return JsonResponse(data)
 
 def get_page_data_image(request):
     topic = request.GET.get('topic', None)
-
     print('topic-Image:', topic)
 
-    data = {}
-    #downloads the image from the passed url in topic
-    #download_image.download(topic)
+    # downloads the image from the passed url in topic
+    # download_image.download(topic)
 
+    data = {}
     return JsonResponse(data)
 
-import os
 
 def get_page_data_pdf(request):
     url = request.GET.get('topic', None)
     pdf_name = os.path.basename(url)
 
-    data = {}
-
     # downloads pdf from right click
-    download_pdf.download_file(url, pdf_name)
+    pdf_path = download_pdf.download_file(url, pdf_name)
 
     # convert its table(s) into csv(s)
-    download_pdf.get_csv_table_from_pdf(pdf_name)
+    csv_path = download_pdf.download_pdf_tables(pdf_path)
 
+    data = {}
     return JsonResponse(data)
