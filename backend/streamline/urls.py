248c97b514fcfc860e9d95eff043871f572f7a9b
Aidanddow <2468915D@student.gla.ac.uk>
2022-02-09 11:48:56 +0000

    Fixed issue with app name and urls

--- backend/streamline/urls.py
+++ backend/streamline/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'get_page_data_HTML/$', views.get_page_data_HTML, name='get_page_data_HTML'),
-    url(r'get_page_data_pdf/$', views.get_page_data_pdf, name='get_page_data_pdf'),
+    path('get_page_data_HTML/', views.get_page_data_HTML, name='get_page_data_HTML'),
+    path('get_page_data_pdf/', views.get_page_data_pdf, name='get_page_data_pdf'),
     path('download_page/<int:url_id>/<int:table_id>', views.download_page, name='download_page'),
 ]
